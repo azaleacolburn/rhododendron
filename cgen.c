22f18193f5436c6be0779a5cc52f1404fad62d79
Elo Colburn <elocolburn@comcast.net>
2023-08-21 20:40:45 -0300

    feat: added ","

--- cgen.c
+++ cgen.c
@@ -6,8 +6,12 @@
 #include"register.h"
 
 char* expr_code_gen(RegisterTracker* reg, TokenNode* node) {
+    // Ok so this is hyper specific
     char* reg = itoa(assign_register(reg, REG_W));
     char* ret = strcat("mov ", reg);
+    ret = strcat(ret, ", ");
+    ret = strcat(ret, (char*)node->token->value);
+    printf("ret: %s", ret);
     free(reg);
     free(ret);
     return ret;
