6d507ed7be279803fd60e3cb2ea6db4fc24b11d9
Elo Colburn <elocolburn@comcast.net>
2023-09-07 19:52:10 -0700

    learning: finally prints hello world

--- arm_testing/expr.s
+++ arm_testing/expr.s
@@ -1,17 +1,29 @@
 .global _main
 .balign 4
+
 _main:
-    mov w0, 1
-    mov w1, 1
+    // str sp, []
+    b _print
+    b _terminate
 
-    ret
-
-print:
+/* The byte to be printed is placed on the stack as an arg */
+_print:
     mov  x0,  #1                   /* the print part */
-    mov  x1,  sp                   /* XXX x1 points to the byte to be written */
-    mov  x2,  #1
-    mov  w8,  #64
+    // mov x1,
+    adr x1, helloworld
+    // mov  x1,  sp                   /* x1    points to the byte to be written */
+    mov  x2,  #12
+    mov  x16,  #4
     svc  #0
-exit:                                  /* XXX */
-    add  sp, sp, #16               /* XXX restore stack before returning */
-    ret  
+
+_reboot:
+    mov x0, #1 // instant reboot
+    mov x16, #55 // reboot
+    svc 0 // syscall
+_terminate:                              
+    // add  sp, sp, #16               /* restore stack before returning */
+    mov x0, #0 // return 0
+    mov x16, #1 // terminate
+    svc 0 // syscall
+
+helloworld: .ascii "hello world\n"
