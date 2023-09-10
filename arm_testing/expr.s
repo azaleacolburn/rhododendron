e0addfe6639d0bedc836881cdfd58276f0f212fb
Elo Colburn <elocolburn@comcast.net>
2023-09-10 08:09:05 -0700

    learning: about str and ldr

--- arm_testing/expr.s
+++ arm_testing/expr.s
@@ -2,28 +2,41 @@
 .balign 4
 
 _main:
-    // str sp, []
-    b _print
+    adr x1, helloworld
+    mov x2, #12
+    ; b _print
+    ; b _expr
+    b _reboot
     b _terminate
 
-/* The byte to be printed is placed on the stack as an arg */
+_expr:
+    mov x9, #2
+    mov x10, #2
+    add x7, x10, x9
+    str x7, [x1]
+    mov x2, #1
+    b _print
+
+// The byte to be printed will be placed on the stack as an arg
+// Args: 
+// - x1: the bytes to be written
+// - x2: the number of bytes to be written
 _print:
-    mov  x0,  #1                   /* the print part */
-    // mov x1,
-    adr x1, helloworld
-    // mov  x1,  sp                   /* x1    points to the byte to be written */
-    mov  x2,  #12
-    mov  x16,  #4
-    svc  #0
+    mov  x0,  #1                      // the print part 
+    // adr x1, helloworld
+    // mov  x1,  sp                   // x1 points to the byte to be written
+    //mov  x2,  #12                     // The byte length of the buffer(or sp later)
+    mov  x16,  #4                     // syscall will be a print
+    svc  #0                           // syscall
 
 _reboot:
-    mov x0, #1 // instant reboot
-    mov x16, #55 // reboot
-    svc 0 // syscall
+    mov x0, #1                        // instant reboot
+    mov x16, #55                      // reboot the machine
+    svc 0                             // syscall
 _terminate:                              
-    // add  sp, sp, #16               /* restore stack before returning */
-    mov x0, #0 // return 0
-    mov x16, #1 // terminate
-    svc 0 // syscall
+    // add  sp, sp, #16               // restore stack before returning
+    mov x0, #0                        // return 0
+    mov x16, #1                       // terminate
+    svc #0                             // syscall
 
 helloworld: .ascii "hello world\n"
