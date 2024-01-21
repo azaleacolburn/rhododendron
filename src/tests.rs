3b4bfe14a5340f475284dd19fa09b2d2af11d0cc
Azalea Colburn <elocolburn@comcast.net>
2024-01-20 23:29:12 -0800

    refactor: test suite

--- src/tests.rs
+++ src/tests.rs
@@ -2,41 +2,41 @@ use crate::{code_gen::main, lexer::string_to_tokens, parser::program};
 use std::{
     fs, io,
     path::{Path, PathBuf},
+    process::Command,
     str,
 };
 
 #[test]
 fn test_all() {
-    let tests: Vec<PathBuf> = match get_all_files(&Path::new("tests/rh/")) {
-        Ok(tests) => tests,
-        Err(_) => panic!("No files to be tests"),
-    };
-    let validation: Vec<PathBuf> = match get_all_files(&Path::new("tests/asm/")) {
-        Ok(validation) => validation,
-        Err(_) => panic!("No validation files"),
-    };
-    assert_eq!(tests.len(), validation.len());
+    let tests: Vec<PathBuf> = get_all_files(&Path::new("tests/rh/")).expect("No files to be tests");
+    let validation: Vec<PathBuf> =
+        get_all_files(&Path::new("tests/validation")).expect("No files for validation");
 
+    assert_eq!(tests.len(), validation.len());
     for test_i in 0..tests.len() {
         let raw_src = fs::read(tests[test_i].clone()).unwrap();
         let src = str::from_utf8(&raw_src).unwrap();
+        let names: String = tests[test_i]
+            .file_name()
+            .unwrap()
+            .split(".")
+            .collect::<&str>();
+        let valid = str::from_utf8(&fs::read(validation[test_i].clone()).unwrap());
 
-        let tokens = match string_to_tokens(src) {
-            Ok(tokens) => tokens,
-            Err(_) => panic!("Unable to parse integer literal"),
-        };
-        let node = match program(tokens) {
-            Ok(node) => node,
-            Err(err) => panic!("Failed Parsing: {:?}", err),
-        };
+        let (tokens, line_tracker) =
+            string_to_tokens(src).expect("Unable to parse integer literal");
+        let node = program(tokens, line_tracker).unwrap();
         let generated_asm = main(&node);
 
-        let valid_raw =
-            fs::read(validation[test_i].clone()).expect("a corresponding validation test");
-        let valid = str::from_utf8(&valid_raw).unwrap().to_string();
-        println!("{}", valid);
-        println!("{}", generated_asm);
-        assert_eq!(generated_asm, valid);
+        Command::new(format!("gcc -o {}.asm {}", names[0], names[0]))
+            .spawn()
+            .expect("Assembly failed");
+
+        let generated_output = Command::new(format!("./{}", names[0]))
+            .output()
+            .expect("Failed at runtime");
+
+        assert_eq!(generated_output, valid);
     }
 }
 
