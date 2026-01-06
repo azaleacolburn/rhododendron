6cae20f5a7ec476fe9287adbcd6713c2e48ac327
Azalea Colburn <azaleacolburn@gmail.com>
2026-01-05 17:07:16 -0800

    chore: compile script

---
+++ compile.sh
@@ -0,0 +1,4 @@
+cargo run -- $1.rh $1.asm 
+as -arch arm64 -o $1.o $1.asm
+ld -o $1 $1.o -lSystem -syslibroot `xcrun -sdk macosx --show-sdk-path` -e .main -arch arm64
+rm $1.o
--- test_rh.sh
+++
@@ -1,4 +0,0 @@
-cargo run -- tests/core/$1.rh gen/core/$1.asm 
-as -arch arm64 -o $1.o gen/core/$1.asm
-ld -o gen/core/$1 $1.o -lSystem -syslibroot `xcrun -sdk macosx --show-sdk-path` -e .main -arch arm64
-rm $1.o
